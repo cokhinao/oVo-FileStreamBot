6f1aae7f1975530ab95abeff4128205b73b4842d
Có Khi Nào <143844176+cokhinao@users.noreply.github.com>
2023-09-04 18:36:27 +0700

    Update vars.py

--- main/vars.py
+++ main/vars.py
@@ -8,13 +8,13 @@ load_dotenv()
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(environ.get("API_ID"))
-    API_HASH = str(environ.get("API_HASH"))
-    BOT_TOKEN = str(environ.get("BOT_TOKEN"))
+    API_ID = int(environ.get("API_ID","28106063"))
+    API_HASH = str(environ.get("API_HASH","1cfaa68c86f6f7f305f5d470891d604a"))
+    BOT_TOKEN = str(environ.get("BOT_TOKEN","6328294956:AAG3cO2B9qvpdNEv-1hDhbjC39FCbd4Gtk4"))
     SLEEP_THRESHOLD = int(environ.get("SLEEP_THRESHOLD", "60"))  # 1 minute
     WORKERS = int(environ.get("WORKERS", "6"))  # 6 workers = 6 commands at once
     BIN_CHANNEL = int(
-        environ.get("BIN_CHANNEL", None)
+        environ.get("BIN_CHANNEL", "-1001916539480")
     )  # you NEED to use a CHANNEL when you're using MULTI_CLIENT
     PORT = int(environ.get("PORT", 8080))
     BIND_ADDRESS = str(environ.get("WEB_SERVER_BIND_ADDRESS", "0.0.0.0"))
@@ -29,19 +29,19 @@ class Var(object):
     else:
         ON_HEROKU = False
     FQDN = (
-        str(environ.get("FQDN", BIND_ADDRESS))
+        str(environ.get("FQDN", stream-driver.onrender.com))
         if not ON_HEROKU or environ.get("FQDN")
         else APP_NAME + ".herokuapp.com"
     )
     if ON_HEROKU:
         URL = f"https://{FQDN}/"
     else:
-        URL = "http{}://{}{}/".format(
+        URL = "https{}://{}{}/".format(
             "s" if HAS_SSL else "", FQDN, "" if NO_PORT else ":" + str(PORT)
         )
 
-    UPDATES_CHANNEL = "HxBots"
-    OWNER_ID = int(environ.get('OWNER_ID', '754495556'))
+    UPDATES_CHANNEL = "CGR_CL"
+    OWNER_ID = int(environ.get('OWNER_ID', '5892897129'))
 
     BANNED_CHANNELS = list(set(int(x) for x in str(environ.get("BANNED_CHANNELS", "-1001296894100")).split()))
     BANNED_USERS = list(set(int(x) for x in str(environ.get("BANNED_USERS","5275470552 5287015877")).split()))
